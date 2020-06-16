46f02fdb450eb9437411c5e78d834181b9a3c476
U-DESKTOP-ULDMN9K\hcq <494268393@qq.com>
2020-06-16 09:59:31 +0800

    add c-lexer

--- Lexer/c-lexer.cc
+++ Lexer/c-lexer.cc
@@ -1,3 +1,58 @@
 #include "c-lexer.h"
 
+CLexer::CLexer() :input_(NULL), start_pos_(0) {
+  init();
+}
 
+int CLexer::init() {
+  std::vector<std::string> regex_rules;
+  std::vector<int> rule_ids;
+  regex_rules.push_back("[_a-zA-Z][_a-zA-Z0-9]*");
+  rule_ids.push_back(IDENTIFIER);
+  regex_rules.push_back("(0x|0X)[0-9a-fA-F]+((u|U)?(l|L)?|(l|L)?(u|U)?)");
+  rule_ids.push_back(INTEGER_CONSTANT);
+  regex_rules.push_back("'([^'\\\\]|\\\\\\\\|\\\\\\?|\\\\'|\\\\\"|(\\\\[abefnrtv])|\\\\([0-7]|[0-7][0-7]|[0-7][0-7][0-7])|\\\\x[0-9a-fA-F]+)'");
+  rule_ids.push_back(CHARACTER_CONSTANT);
+  regex_rules.push_back("([0-9]+.[0-9]*|.[0-9]+|[0-9]+)(e(\\+|\\-)?[0-9]+)?");
+  rule_ids.push_back(REAL_NUMBER_CONSTANT);
+  regex_rules.push_back("\"([^\"]|\\\\\")*\"");
+  rule_ids.push_back(STRING_CONSTANT);
+  regex_rules.push_back("(=|\\+=|\\-=|\\*=|/=|%=|<<=|>>=|&=|\\^=|\\|=)");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back("(\\+\\+|\\-\\-)");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back("(\\+|\\-|\\*|/|%)");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back("(==|!=|<|>|<=|>=)");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back("(&&|(\\|\\|)|!)");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back("(<<|>>)");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back("(&|(\\|)|(\\^)|~)");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back(",");
+  rule_ids.push_back(); regex_rules.push_back("(\\(|\\)|\\[|\\]|{|}|;|,|.|:)");
+  rule_ids.push_back(SEPARATOR);
+  regex_rules.push_back("(.|(\\->))");
+  rule_ids.push_back(OPERATOR);
+  regex_rules.push_back("(\\?|:)");
+  rule_ids.push_back(SEPARATOR);
+  regex_rules.push_back("[ \t\n\v\f]+");
+  rule_ids.push_back(WHITE_SPACE);
+  lexer.compile(regex_rules, rule_ids);
+
+  return 0;
+}
+
+int CLexer::init_input(const char *input_text) {
+  input_ = input_text;
+  return 0;
+}
+
+int CLexer::next_token(Token *tok) {
+  assert(tok != NULL);
+
+  
+  return 0;
+}
--- Lexer/c-lexer.h
+++ Lexer/c-lexer.h
@@ -3,14 +3,35 @@
 
 #include "lexer.h"
 
+// TODO add type KEYWORD
+enum LexType {
+  IDENTIFIER,
+  INTEGER_CONSTANT,
+  CHARACTER_CONSTANT,
+  REAL_NUMBER_CONSTANT,
+  STRING_CONTANT,
+  OPERATOR,
+  SEPARATOR,
+  WHITE_SPACE
+};
+
 class Token {
+  LexType type,
+  int start,
+  int end
 };
 
 class CLexer {
 public:
-  int init();
+  int init_input(const char *input_text, int text_size);
   int next_token(Token *tok);
 private:
+  int init();
+private:
+  Lexer lexer_;
+  const char *input_;
+  int start_pos_;
+  int text_len_;
 };
 
 #endif
